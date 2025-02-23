44f56e76809821b8e1719c3e44901f266a63d049
hyunsik <hora5225@gmail.com>
2025-02-23 23:13:02 +0900

    250222 프로그래머스 프로세스 Lv2 - 스택/큐 연습 문제 풀이 다르게

--- src/main/java/org/example/programmers/프로세스.java
+++ src/main/java/org/example/programmers/프로세스.java
@@ -23,44 +23,64 @@ public class 프로세스 {
         //int[] arr = new int[priorities.length];
         //int[] arr = Arrays.stream(priorities).sorted().toArray();
         //arr을 역순으로
-        //Arrays.sort(priorities);
+
+        //priorities를 차례대로 큐에 입력
+        Queue<int[]> queue2 = new LinkedList<>();
+        for (int i = 0; i < priorities.length; i++) {
+            queue2.add(new int[]{i, priorities[i]});
+        }
+        //queue2 출력
+        System.out.println("queue2 = " + queue2);
+
         //배열 역순 정렬
         int[] arr = new int[priorities.length];
-        for(int i = 0; i < priorities.length; i++){
+
+        Arrays.sort(priorities);
+
+        for (int i = 0; i < priorities.length; i++) {
             arr[i] = priorities[priorities.length - i - 1];
         }
 
         //arr을 차례대로 큐에 입력
         Queue<Integer> queue = new LinkedList<>();
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             queue.add(arr[i]);
         }
-        //queue 출력
-        System.out.println(queue);
 
-        //priorities를 차례대로 큐에 입력
-        Queue<int[]> queue2 = new LinkedList<>();
-        for(int i = 0; i < priorities.length; i++){
-            queue2.add(new int[]{i, priorities[i]});
-        }
-        //queue2 출력
-        System.out.println(queue2);
+        //queue 출력.
+        System.out.println("queue = " + queue);
 
-        while(!queue.isEmpty()){
+
+
+        int cnt = 0;
+        while (!queue.isEmpty()) {
+            if (answer != 0) {
+                break;
+            }
             int temp = queue.poll();
 
-
+            while (true) {
+                int[] temp2 = queue2.poll();
+                if (temp == temp2[1]) {
+                    cnt++;
+                    if (temp2[0] == location) {
+                        answer = cnt;
+                    }
+                    break;
+                } else {
+                    queue2.add(temp2);
+                }
+            }
         }
-
 
 
         return answer;
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         프로세스 s = new 프로세스();
         System.out.println(s.solution(new int[]{2, 1, 3, 2}, 2));
-        System.out.println(s.solution(new int[]{1,1,9,1,1,1}, 0));
+        System.out.println(s.solution(new int[]{1, 1, 9, 1, 1, 1}, 0));
     }
 
 }
