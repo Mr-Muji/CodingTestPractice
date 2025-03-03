026bcf10196ff7c32a8301adc12829ed4c456f57
hyunsik <hora5225@gmail.com>
2025-03-03 11:17:59 +0900

    250303 파이썬 시작

---
+++ src/main/python/programmers/기초/정수부분.py
@@ -0,0 +1,15 @@
+'''
+정수부분
+프로그래머스 기초
+https://school.programmers.co.kr/learn/courses/30/lessons/181850
+'''
+
+def solution(flo):
+    answer = 0
+
+    answer = int(flo)
+
+    return answer
+
+print(solution(1.42))
+print(solution(69.32))
