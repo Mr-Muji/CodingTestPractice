ce0f3f821f4bc9320762edc5ac16c27dc0f46997
hyunsik <hora5225@gmail.com>
2025-03-15 18:57:05 +0900

    250315

---
+++ src/main/python/programmers/하노이의탑.py
@@ -0,0 +1,10 @@
+"""
+250315
+하노이의 탑
+프로그래머스 Lv2
+https://school.programmers.co.kr/learn/courses/30/lessons/12946?language=python3
+
+"""
+def solution(n):
+    answer = [[]]
+    return answer
---
+++ src/main/python/test/3월테스트/3번.py
@@ -0,0 +1,11 @@
+"""
+3번 문제
+https://kakao-tech-bootcamp.goorm.io/learn/lecture/55237/%ED%8C%90%EA%B5%90-2%EA%B8%B0-%EC%B9%B4%EC%B9%B4%EC%98%A4-%ED%85%8C%ED%81%AC-%EB%B6%80%ED%8A%B8%EC%BA%A0%ED%94%84-%EC%BD%94%EB%94%A9-%ED%85%8C%EC%8A%A4%ED%8A%B8-%EA%B0%95%EC%A2%8C/lesson/2547540/3%EB%B2%88
+
+"""
+
+import sys
+input = sys.stdin.readlines
+
+K = int(input().strip())
+
